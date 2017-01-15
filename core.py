e75e4e2f809518d8e487e9c74fb98c0e4aa0b235
Darren Rainey <darren@darrenraineys.co.uk>
2017-01-15 16:31:40 +0000

    Core AI Functions V1.0A

---
+++ core.py
@@ -0,0 +1,30 @@
+# AI Core v3 By Darren Rainey
+import os
+from time import gmtime, strftime
+import random
+
+while True:
+	Greetings = ['Hello', 'Hey', 'Greetings']
+	userInput = raw_input(">>> ")
+	if userInput in ['hi', 'HI', 'Hi', 'Hello', 'hello', 'helo']:
+		print (Greetings[int(random.random() * len(Greetings))])
+		#print random.choice(Greetings)("AI: ")
+		os.system("espeak Hello_$USER >/dev/null 2>&1 &")
+    
+	elif userInput  in ['time', 'Time', 'TIME']:
+		os.system("echo AI: The time is $(date +'%T %p')")
+		os.system("espeak The_time_is_$(date +'%T %p') >/dev/null 2>&1 &")
+    
+	elif userInput in ['exit', 'Exit', 'quit', 'Quit']:
+		print("Shutting Down")
+		os.system("espeak Shutting_Down_Core_Functions >/dev/null 2>&1 &")
+		exit()
+    
+	elif userInput in ['HELP', 'Help', 'help']:
+		print("AI Core V3.0A By Darren Rainey")
+		print("Help	-	Displays this menu")
+		print("Exit	-	Exits the program")
+		print("Hello	-	Greeting")
+    
+	else:
+		print("I did not understand what you said")
