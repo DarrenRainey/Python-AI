2a6502c9e502553390823890e27d2ea81592a3c3
Darren Rainey <darren@darrenraineys.co.uk>
2017-01-17 19:15:48 +0000

    Added Unique Lookup ID'S to allow faster lookup of functions

--- core.py
+++ core.py
@@ -1,33 +1,39 @@
-# AI Core v3 By Darren Rainey
-import os
-from time import gmtime, strftime
-import random
+	# AI Core v3 By Darren Rainey
+	import os
+	from time import gmtime, strftime
+	import random
 
-while True:
-	Greetings = ['Hello', 'Hey', 'Greetings']
-	userInput = raw_input(">>> ")
-	if userInput in ['hi', 'HI', 'Hi', 'Hello', 'hello', 'helo']:
-		print (Greetings[int(random.random() * len(Greetings))])
-		#print random.choice(Greetings)("AI: ")
-		os.system("espeak Hello_$USER >/dev/null 2>&1 &")
-    
-	elif userInput  in ['time', 'Time', 'TIME']:
-		os.system("echo AI: The time is $(date +'%T %p')")
-		os.system("espeak The_time_is_$(date +'%T %p') >/dev/null 2>&1 &")
-    
-	elif userInput in ['exit', 'Exit', 'quit', 'Quit']:
-		print("Shutting Down")
-		os.system("espeak Shutting_Down_Core_Functions >/dev/null 2>&1 &")
-		exit()
-    
-	elif userInput in ['HELP', 'Help', 'help']:
-		print("AI Core V3.0A By Darren Rainey")
-		print("Help	-	Displays this menu")
-		print("Exit	-	Exits the program")
-		print("Hello	-	Greeting")
-		print("Time	-	Displays the current time")
-    
-	else:
-		Error = ['What was that ?', 'I"m sorry I did not understand you', 'Excuse me, What was that ?', 'Please say that again im not sure what you mean']
-		print (Error[int(random.random() * len(Error))])
-		#print("I did not understand what you said")
+	while True:
+		Greetings = ['Hello', 'Hey', 'Greetings']
+		userInput = raw_input(">>> ")
+
+		# IDA0001 Hello/Greeting Function
+		if userInput in ['hi', 'HI', 'Hi', 'Hello', 'hello', 'helo']:
+			print (Greetings[int(random.random() * len(Greetings))])
+			#print random.choice(Greetings)("AI: ")
+			os.system("espeak Hello_$USER >/dev/null 2>&1 &")
+
+		# IDA0002 Time Function	
+		elif userInput  in ['time', 'Time', 'TIME']:
+			os.system("echo AI: The time is $(date +'%T %p')")
+			os.system("espeak The_time_is_$(date +'%T %p') >/dev/null 2>&1 &")
+
+		# IDA0003 Exit Function
+		elif userInput in ['exit', 'Exit', 'quit', 'Quit']:
+			print("Shutting Down")
+			os.system("espeak Shutting_Down_Core_Functions >/dev/null 2>&1 &")
+			exit()
+
+		# IDA0004 Help/Guide Function
+		elif userInput in ['HELP', 'Help', 'help']:
+			print("AI Core V3.0A By Darren Rainey")
+			print("Help	-	Displays this menu")
+			print("Exit	-	Exits the program")
+			print("Hello	-	Greeting")
+			print("Time	-	Displays the current time")
+
+		# IDA0005 Error Function
+		else:
+			Error = ['What was that ?', 'I"m sorry I did not understand you', 'Excuse me, What was that ?', 'Please say that again im not sure what you mean']
+			print (Error[int(random.random() * len(Error))])
+			#print("I did not understand what you said")
