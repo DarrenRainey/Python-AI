38a752a5e2f6ea33484026c0c61ac89d72865821
Darren Rainey <darren@darrenraineys.co.uk>
2017-01-20 13:11:09 +0000

    Added Labels And RAM Usage Function

--- External/Status.sh
+++ External/Status.sh
@@ -1,1 +1,4 @@
+# CPU USAGE
 top -bn1 | grep "Cpu(s)" |            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" |            awk '{print 100 - $1"% CPU USAGE"}'
+# RAM USAGE
+free | grep Mem | awk '{print $3/$2 * 100.0 "% RAM USAGE"}'
